f8cd8bd22bd858f47c6330b10aaab0b6848bfe54
ksw <tlsdnddl12@naver.com>
2022-08-12 03:12:26 +0900

    [Add] react-practice

---
+++ react-practice/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>My Todos</h1>
+        <div className="card">
+          <h2>TITLE</h2>
+          <div className="actions">
+            <button className="btn">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
