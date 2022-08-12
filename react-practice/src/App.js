5dff159fff76ab681df551b564f83edcd182c70a
ksw <tlsdnddl12@naver.com>
2022-08-12 23:22:41 +0900

    [Add] made Todo component

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,15 +1,11 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
-      <div>
-        <h1>My Todos</h1>
-        <div className="card">
-          <h2>TITLE</h2>
-          <div className="actions">
-            <button className="btn">Delete</button>
-          </div>
-        </div>
-      </div>
+      <h1>My Todos</h1>
+      <Todo text="hello" />
+      <Todo text="hi" />
     </div>
   );
 }
