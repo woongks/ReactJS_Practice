088b9bdad8988f20d0120927a888f6c5f279bca1
ksw <tlsdnddl12@naver.com>
2022-08-15 03:40:34 +0900

    [Add] useState

--- react-practice/src/components/Todo.js
+++ react-practice/src/components/Todo.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   function deleteHandler() {
-    console.log("clicked");
-    console.log(props.text);
+    setModalIsOpen(true);
+  }
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
   return (
     <div className="card">
@@ -11,6 +18,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
