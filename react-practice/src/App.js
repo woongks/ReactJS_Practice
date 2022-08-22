545fb198c6890864805b7ff283ba11a5d79519ba
ksw <tlsdnddl12@naver.com>
2022-08-22 22:42:42 +0900

    [Add] todo app

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,5 +1,78 @@
-function App() {
-  return <div></div>;
-}
+import React, { Component } from "react";
+import "./App.css";
 
-export default App;
+export default class App extends Component {
+  state = {
+    todoData: [
+      { id: "1", title: "공부하기", completed: true },
+      { id: "2", title: "청소하기", completed: false },
+    ],
+    value: "",
+  };
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
+  handleClick(id) {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    this.setState({ todoData: newTodoData });
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ value: e.target.value });
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+          {this.state.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <p>
+                <input type="checkbox" defaultChecked={false} />
+                {data.title}
+                <button
+                  style={this.btnStyle}
+                  onClick={() => this.handleClick(data.id)}>
+                  x
+                </button>
+              </p>
+            </div>
+          ))}
+          <form style={{ display: "flex" }}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
